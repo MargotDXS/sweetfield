97a5f6f6252de8804bae7e6fcb0d87af9adedb23
MargotDXS <margot@douxchamps.ch>
2023-03-30 15:10:51 +0200

    displays header on all pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import Cheader from './components/parts/Cheader';
 import Cfooter from './components/parts/Cfooter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Cheader />
     <Cfooter />
   </React.StrictMode>
 );
