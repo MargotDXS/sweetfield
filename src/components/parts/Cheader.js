3a61ce0a3ea48f6978fbae6bd636a5ccb8ca1040
MargotDXS <margot@douxchamps.ch>
2023-03-30 16:11:58 +0200

    adds burger menu logic

--- src/components/parts/Cheader.js
+++ src/components/parts/Cheader.js
@@ -3,18 +3,29 @@ import './Cheader.scss';
 
 class Cheader extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            isMenuOpen: false,
+        }
     }
+
+    toggleMenu = () => {
+        this.setState({
+          isMenuOpen: !this.state.isMenuOpen
+        });
+      };
+      
     render() {
         return(
             <header>
                 <nav>
                     <figure>logo</figure>
-                    <ul>
+                    <ul className={this.state.isMenuOpen ? 'active' : ''}>
                         <li>menu 1</li>
                         <li>menu 2</li>
                         <li>menu 3</li>
                     </ul>
+                    <button className="burgerMenu" onClick={this.toggleMenu}> burger </button>
                 </nav>
                 <hr/>
             </header>
