8535b2c1ae2fa5aae0ac7e35904bfcd07bf97cd6
MargotDXS <margot@douxchamps.ch>
2023-03-30 14:58:06 +0200

    displays footer on all pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import Cfooter from './components/parts/Cfooter';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*   application component  */}
+    
+    <Cfooter />
   </React.StrictMode>
 );
 
