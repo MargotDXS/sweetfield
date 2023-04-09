70a2170d1c192064f12a201fe6fa56cef6321f41
Margot-Douxchamps <89386605+Margot-Douxchamps@users.noreply.github.com>
2023-04-09 16:38:02 +0200

    animates heroHeader

--- src/components/moleculs/HeroHeader.js
+++ src/components/moleculs/HeroHeader.js
@@ -8,9 +8,13 @@ class HeroHeader extends React.Component {
     render() {
         return(
             <section className="heroHeader">
-                <h1>Je m'appelle <span className="emph">Margot Douxchamps</span></h1>
+                <div className="heroHeaderFrame">
+                    <h1>Je m'appelle <span className="emph">Margot Douxchamps</span></h1>
+                </div>
                 <br />
-                <h1>Je suis <span className="emph">créative multidisciplinaire</span></h1>
+                <div className="heroHeaderFrame">
+                    <h1>Je suis <span className="emph">créative multidisciplinaire</span></h1>
+                </div>
                 <h4>J'ai 21 ans et suis basée en Suisse mais travail volontiers pour des projets à l'étranger. Ouvertures d'esprit et curiosité sont mes principales valeurs.</h4>
             </section>
         )
