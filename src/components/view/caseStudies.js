96a63924c64da9a074d548ddcd248020883e57c9
MargotDXS <margot@douxchamps.ch>
2023-04-28 10:04:03 +0200

    adds text presentation on case studies page

--- src/components/view/caseStudies.js
+++ src/components/view/caseStudies.js
@@ -9,6 +9,9 @@ class CaseStudies extends React.Component {
     render() {
         return(
             <>
+            <main className="gridContainer caseStudies">
+                <h4 className="presentation">Voilà une liste de quelques projets que j'ai réalisé. Ce n'est pas une liste exhaustive mais elle te donnera une idée de ce que je peux faire.</h4>
+            </main>
             <div className="bg">
                 <div className="grainyBg whiteBlended"></div>
             </div>
